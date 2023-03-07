d8b2cd79475d31616b0fab0f811cfbe0bb461468
jesniey11 <jesniey11@gmail.com>
2023-03-08 08:20:30 +0900

    savw

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private Animator animator;
-    private Rigidbody rigidbody;
+    private Rigidbody rigid;
     private Vector3 moveDir;
 
     private Transform cameraArm;
@@ -33,20 +34,66 @@ public class PlayerController : MonoBehaviour
     // WASD 이동 (카메라가 보는 방향 기준)
     private void Move()
     {
-        if (!playerControl.isDead) {
-            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), )
+        if (!PlayerMain.isDead) {
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            bool isMove = moveInput.magnitude != 0;
+            animator.SetBool("isMove", isMove);
+
+            if (isMove && !PlayerMain.isJump) {
+                Vector3 lookDir = new Vector3(cameraArm.forward.x, 0.0f, cameraArm.forward.z);
+                Vector3 lookRight = new Vector3(cameraArm.right.x, 0.0f, cameraArm.right.z).normalized;
+                moveDir = (lookDir * moveInput.y) + (lookRight * moveInput.x);
+
+                playerBody.forward = moveDir;
+            }
+
+            transform.position += moveDir * Time.deltaTime * playerSpeed;
         }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!PlayerMain.isJump) {
+				PlayerMain.isJump = true;
+				rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+            else { return; }
+        }
+    }
+
+    //GameManager로 분리예정
+    private void Dead() {
+        player.SetActive(false);
+		PlayerMain.isDead = true;
+
+        // 5초뒤 부활 - 버튼 눌러야 부활하게 바꿀까?
+        Invoke("Respawn", 5);
+    }
+
+    private void Respawn() {
+		PlayerMain.isDead = false;
+        //플레이어 진행도에 따라 랜덤 리스폰 장소(하드코딩X)로 이동
+        //임시로 0 0 0으로 이동하게 해둠
+        transform.position = new Vector3(0, 0, 0);
+
+        //무적?
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = playerBody.GetComponent<Animator>();
+		rigid = playerBody.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraRotate();
+        Move();
+        Jump();
+
+        if(PlayerMain.isDead) { Dead(); }
     }
 }
---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour
+{
+    public static bool isJump = false;
+    public static bool isDead = false;
+
+    public static float hpMaxNow = 0.0;
+    public static float hpNow = 0.0;
+
+    public override bool SetHp(float hp, float hpMax) {
+        if (hp > hpMax) { hp = hpMax; }
+        hpNow = hp;
+        hpMaxNow = hpMax;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    
+}
